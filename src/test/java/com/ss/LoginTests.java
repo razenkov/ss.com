8555d1d21c0d95572e27ce887a8999ee24f36ce0
mymac <7772121>
2018-02-18 22:57:48 +0200

    @Step practice

--- src/test/java/com/ss/LoginTests.java
+++ src/test/java/com/ss/LoginTests.java
@@ -2,6 +2,7 @@ package com.ss;
 
 import com.ss.core.WebDriverTestBase;
 import com.ss.pages.LoginPage;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -9,6 +10,8 @@ import org.testng.annotations.Test;
 
 public class LoginTests extends WebDriverTestBase{
     @DataProvider(name = "loginCredentials")
+
+
     public Object[][] userCredentials(){
         return new Object[][]{
                 {"email", "password"},
@@ -17,6 +20,7 @@ public class LoginTests extends WebDriverTestBase{
         };
     }
     @Test(dataProvider = "loginCredentials")
+    @Step("!!!HERE IM TRYING TO ADD @STEP -> {email} <- TO REPORT.")
     public void loginTest(String email, String pass){
         driver.get("https://www.ss.com/ru/login/");
         LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
